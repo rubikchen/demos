1ce4abbbc713bc50e5dfefa9b08e9cca20e4268f
rubikchen <rubikchen@gmail.com>
2024-03-27 20:15:16 +0800

    threadpoll with stack size.

--- cpp11/threadpool/test/test_threadpool.cc
+++ cpp11/threadpool/test/test_threadpool.cc
@@ -1,23 +1,26 @@
 #include <iostream>
 
 #include "threadpool.h"
+#include "threadpool_stack.h"
 
-void good(int a) {
-  std::cout << __LINE__ << " " << a << std::endl;
-}
+void good(int a) { std::cout << __LINE__ << " " << a << std::endl; }
 
-class Test{
-public:
-  void show() {
-    std::cout << __func__ << std::endl;
-  }
+class Test {
+ public:
+  void show() { std::cout << __func__ << std::endl; }
 };
 
-int main() {
-  cpp11::threadpool ts(2);
-	ts.enqueue(good, 2);
+template <typename T>
+void test(T&& ts) {
+  ts.enqueue(good, 2);
   Test t;
   ts.enqueue(&Test::show, &t);
 
-  std::cout << ts.enqueue([](int answer) { return answer; }, 42).get() << std::endl;
+  std::cout << ts.enqueue([](int answer) { return answer; }, 42).get()
+            << std::endl;
+}
+
+int main() { 
+  test(cpp11::threadpool(2)); 
+  test(cpp11::threadpool_stack(2)); 
 }
