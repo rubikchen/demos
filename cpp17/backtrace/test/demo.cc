39de63306b27256eef0fa7df5a54f2b4e4be2b4d
rubikchen <rubikchen@gmail.com>
2024-04-20 07:44:21 +0800

    async exception.

--- cpp17/backtrace/test/demo.cc
+++ cpp17/backtrace/test/demo.cc
@@ -1,20 +1,22 @@
 #include <signal.h>
 
-#include<iostream>
-#include<string>
-
+#include <future>
+#include <iostream>
+#include <string>
 
 #include "./backtrace.h"
 
 void show(int) {
-    auto trace = BackTrace::GetBackTrace();
-    for (const auto& item: trace) {
-        std::cout << item << std::endl;
-    }
+  auto trace = BackTrace::GetBackTrace();
+  for (const auto& item : trace) {
+    std::cout << item << std::endl;
+  }
 }
 
 int main() {
-    signal(SIGABRT, show);
+  signal(SIGABRT, show);
 
-    throw 1;
+  // if not set noexcept function attr, you will not get an exception at the
+  // place it throw, rather than future get was invoked.
+  std::async(std::launch::async, []() noexcept { std::abort(); }).get();
 }
